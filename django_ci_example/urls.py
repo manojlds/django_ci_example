df702c60056bba53b171a5c9a6d126d2331b6d4e
manojlds <manojlds@gmail.com>
2014-01-11 17:35:38 +0530

    Adding views

--- django_ci_example/urls.py
+++ django_ci_example/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_ci_example.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
