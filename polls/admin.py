00552efddaf49e36c38721fc9c35ac277adf1007
manojlds <manojlds@gmail.com>
2014-01-11 17:16:15 +0530

    Adding Poll and Choice to admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from polls.models import Poll, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    search_fields = ['question']
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
@@ -6,6 +8,9 @@ class Poll(models.Model):
 
     def __unicode__(self):
         return self.question
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
