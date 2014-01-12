821ec898104460b4d954e03aa81fb2b8363ecd09
manojlds <manojlds@gmail.com>
2014-01-12 20:06:30 +0530

    Fixing flake8 issues

--- django_ci_example/settings/heroku.py
+++ django_ci_example/settings/heroku.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 from .base import *  # noqa
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
